825841259bbeeeac69208d76c5a48b8b45297a54
Dubois Yann <Dubois Yann>
2017-10-04 12:06:00 +0200

    First part not finish

---
+++ Assets/Scripts/InteractiveLine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveLine : MonoBehaviour {
+
+	private List<Vector3> position;
+	public Color color;
+
+	// Use this for initialization
+	void Start () {
+		LineRenderer line = this.gameObject.AddComponent<LineRenderer> ();
+		line.material = new Material(Shader.Find("Particles/Additive"));
+		line.widthMultiplier = 0.2f;
+		position = new List<Vector3> ();
+		//this.setSegment ();
+		this.setCircle(4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LineRenderer line = this.gameObject.GetComponent<LineRenderer> ();
+		line.material.color = color;
+		line.positionCount = position.Count;
+		line.SetPositions (position.ToArray ());
+	}
+
+	void setSegment() {
+		position.Add (new Vector3 (-2,0,0));
+		position.Add (new Vector3 (2,0,0));
+	}
+
+	void setCircle(float r) {
+		float angle = 0;
+		for (int i = 0; i < 30; i++) {
+			position.Add (new Vector3(r * Mathf.Cos(Mathf.Deg2Rad * angle), r * Mathf.Sin(Mathf.Deg2Rad * angle), 0));
+			angle += 360.0f / 30.0f;
+		}
+		position.Add (new Vector3 (r, 0, 0));
+	}
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	public Camera orthoCam;
+	public Camera perspCam;
+	public GameObject section;
+	public GameObject path;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F1)) {
+			orthoCam.enabled = true;
+			perspCam.enabled = false;
+			section.GetComponent<LineRenderer> ().enabled = true;
+			path.GetComponent<LineRenderer> ().enabled = false;
+		} if (Input.GetKeyDown (KeyCode.F2)) {
+			orthoCam.enabled = false;
+			perspCam.enabled = true;
+			section.GetComponent<LineRenderer> ().enabled = false;
+			path.GetComponent<LineRenderer> ().enabled = true;
+		}
+	}
+}
